30e1d36f0c3db1be03f3cf478abe35a4c62fc88c
Brian Barbour <steelvoltage@gmail.com>
2019-05-19 18:05:53 -0400

    Updates

---
+++ precache-manifest.cf572c4a7fdb8a82080d818bcac503ff.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49ea90ed2e5ecd6239f3e5c2d3b0309",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "9b37ddb7cba85435de3c",
+    "url": "/drum-machine/static/css/main.5fa65ab4.chunk.css"
+  },
+  {
+    "revision": "b82b547886b653d6795b",
+    "url": "/drum-machine/static/js/2.f12b2bbc.chunk.js"
+  },
+  {
+    "revision": "9b37ddb7cba85435de3c",
+    "url": "/drum-machine/static/js/main.ce5658fa.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drum-machine/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/drum-machine/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/drum-machine/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/drum-machine/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drum-machine/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drum-machine/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drum-machine/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drum-machine/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/drum-machine/static/media/tom.572ed39d.wav"
+  }
+]);
